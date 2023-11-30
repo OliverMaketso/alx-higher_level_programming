53057175f6fb1f56b7254723da67b3c095d28fae
Oliver Maketso <olivermaketso@gmail.com>
2023-11-30 07:23:02 -0800

    correction of tasks 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,14 +5,10 @@ if __name__ == "__main__":
     b = 5
 
     from calculator_1 import add, sub, mul, div
-    res_add = add(a, b)
-    res_sub = sub(a, b)
-    res_mul = mul(a, b)
-    res_div = div(a, b)
-
+    
     #print the results
-    print("{} + {} = {}".format(a, b, res_add))
-    print("{} + {} = {}".format(a, b, res_sub))
-    print("{} + {} = {}".format(a, b, res_mul))
-    print("{} + {} = {}".format(a, b, res_div))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
 
