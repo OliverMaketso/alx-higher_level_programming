df9670d36562aadf5981a36a259104ec8c82295e
OliverMaketso <122807237+OliverMaketso@users.noreply.github.com>
2023-12-06 00:06:19 +0300

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-    return_list = [num % 2 == 0 for num in my_list]
-    
+    return_list = [num % 2 == 0 for num in my_list]:    
     return (return_list)
